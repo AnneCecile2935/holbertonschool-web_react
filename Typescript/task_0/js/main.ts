5481a666290af0871b5a977a02519b672f0ef852
Annece2935 <anne-cecile.colleter@live.fr>
2025-09-08 14:07:05 +0200

    "feat(task_0): implement Student interface and dynamic table rendering"

--- Typescript/task_0/js/main.ts
+++ Typescript/task_0/js/main.ts
@@ -1,2 +1,44 @@
-console.log("Webpack + TypeScript fonctionnent !");
-document.body.innerHTML = "<h1>Mon projet TypeScript</h1>";
+interface Student {
+	firstName: string;
+	lastName: string;
+	age: number;
+	location: string;
+}
+
+const student1: Student = {
+	firstName: "John",
+	lastName: "Doe",
+	age: 25,
+	location: "New-York",
+};
+
+const student2: Student = {
+  firstName: "Jane",
+  lastName: "Smith",
+  age: 22,
+  location: "Paris",
+};
+
+const studentsList: Student[] = [student1, student2];
+
+function renderTable(students: Student[]): void {
+	const table = document.createElement("table");
+	const headerRow = table.insertRow();
+	const headerCell1 = headerRow.insertCell();
+	const headerCell2 = headerRow.insertCell();
+
+	headerCell1.textContent = "Firstname";
+	headerCell2.textContent = "Location";
+
+	students.forEach((student) => {
+		const row = table.insertRow();
+		const cell1 = row.insertCell();
+		const cell2 = row.insertCell();
+
+		cell1.textContent = student.firstName;
+		cell2.textContent = student.location;
+	});
+
+	document.body.appendChild(table);
+}
+renderTable(studentsList);
